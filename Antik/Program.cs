a93f8e6e7a396a62955be206c7c4690e2ed43f70
BandersGit <andy.i.flaskan@gmail.com>
2020-10-14 09:57:58 +0200

    Initial commit

---
+++ Antik/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Antik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
