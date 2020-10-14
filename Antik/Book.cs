1460b516bac3234bace1dd406b2830221f3ee046
BandersGit <andy.i.flaskan@gmail.com>
2020-10-14 10:54:36 +0200

    Book methods done, creation of Kunder class

--- Antik/Book.cs
+++ Antik/Book.cs
@@ -40,37 +40,78 @@ namespace Antik
                 cursed = false;
             }
 
+            category = categoryList[generator.Next(0, categoryList.Count)];
 
-
+            name = nameList[generator.Next(0, nameList.Count)];
         }
 
         public void PrintInfo()
         {
+            System.Console.WriteLine(name);
+
+            System.Console.WriteLine(rarity);
+
+            System.Console.WriteLine(category);
+
+            System.Console.WriteLine(price);
 
         }
 
-        public int Evaluate()
+        public float Evaluate()
         {
+            float correctPrice = actualValue * rarity;
 
-            return 10;
+            int multiplication = generator.Next (0, 2);
+            if (multiplication == 1)
+            {
+                correctPrice *= 0.5f;
+            }
+            else
+            {
+                correctPrice *= 1.5f;
+            }
+       
+            
+            return correctPrice;
         }
 
         public string GetCategory()
         {
-
-            return "wää";
+            return category;
         }
 
         public string GetName()
         {
-
-            return "name";
+            return name;
         }
 
         public bool IsCursed()
         {
+            int chance = generator.Next(0,5);
 
-            return false;
+            if (cursed == true)
+            {
+                if (chance == 0 || chance == 1 || chance == 2 || chance == 3)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            else if (cursed == false)
+            {
+                if (chance == 0 || chance == 1 || chance == 2 || chance == 3)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
         }
     }
 }
---
+++ Antik/Kunder.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Antik
+{
+    public class Kunder
+    {
+    }
+}
